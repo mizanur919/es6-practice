31fa5eb01df04bf7a6fb90219772f65eeec80c75
mizanur919 <janctgbd@gmail.com>
2021-08-24 02:19:42 +0600

    chaining.js added

--- More ES6/chaining.js
+++ More ES6/chaining.js
@@ -1,11 +1,24 @@
 // Recap Of Destructuring
 // declare variable based on the name of object property
-const myObject = {
-    x: 45,
-    y: 78,
-    z: 60,
-    a: 34,
-    b: 12
-};
-const {x, b, y, a} = myObject;
-console.log(y, x, a, b);
+// Destructuring Object
+// const myObject = {
+//     x: 45,
+//     y: 78,
+//     z: 60,
+//     a: 34,
+//     b: 12
+// };
+// const {x, b, y, a} = myObject;
+// console.log(y, x, a, b);
+
+// Destructuring Array
+// const [p, q] = [4, 8];
+// console.log(q, p);
+
+// const [firstFriend, secondFriend] = ['Hasan Khan', 'Niloy Mahmud'];
+// console.log(`My first friend name is: ${firstFriend} and second friend is: ${secondFriend}`);
+
+
+// Destructuring Objects with other way
+const {flat, memory, sky} = {sky: 'blue', soil:'gray', flat: 'red-green', memory: '8GB'};
+console.log(sky, "\n", flat, "\n", memory);
